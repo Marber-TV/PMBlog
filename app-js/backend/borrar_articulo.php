d73a5694d2910a36bea8ebbd7c11cd9807b82533
Pablo Martinez <117436571+Marber-TV@users.noreply.github.com>
2023-11-30 23:54:19 +0100

    Delete app-js directory

--- app-js/backend/borrar_articulo.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include_once ("config.php");
-$conexion = obtenerConexion();
-$ID = $_POST['id'];
-$consulta = "DELETE FROM articles WHERE article_id = $ID";
-$resultado = mysqli_query($conexion, $consulta);
-responder(null, false, "OK", $conexion); 
